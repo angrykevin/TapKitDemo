44252097d0afbdf2781c60318c9739032361e38f
kevin <hpwasy@gmail.com>
2013-04-12 00:04:03 +0800

    Initial Import.

---
+++ TapKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapKitDemo
+//
+//  Created by Kevin Wu on 4/11/13.
+//  Copyright (c) 2013 Telligenty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate> {
+    UIWindow *_window;
+}
+
+@property(strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapKitDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TapKitDemo
+//
+//  Created by Kevin Wu on 4/11/13.
+//  Copyright (c) 2013 Telligenty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
