1e1e008887f5629d821c0ab2d7a1694f7d11ada9
Kevin <hpwasy@gmail.com>
2013-05-17 15:58:10 +0800

    blah blah blah

---
+++ TapKitDemo/TTMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTMainViewController.h
+//  TapKitDemo
+//
+//  Created by Wu Kevin on 5/17/13.
+//  Copyright (c) 2013 Telligenty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTMainViewController : UIViewController
+
+@end
---
+++ TapKitDemo/TTMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  TTMainViewController.m
+//  TapKitDemo
+//
+//  Created by Wu Kevin on 5/17/13.
+//  Copyright (c) 2013 Telligenty. All rights reserved.
+//
+
+#import "TTMainViewController.h"
+
+@interface TTMainViewController ()
+
+@end
+
+@implementation TTMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
