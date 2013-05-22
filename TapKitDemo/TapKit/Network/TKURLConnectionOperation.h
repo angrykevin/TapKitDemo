309999de713468de83df3410742325bd9714708a
Kevin <hpwasy@gmail.com>
2013-05-22 18:31:49 +0800

    blah blah blah

--- TapKitDemo/TapKit/Network/TKURLConnectionOperation.h
+++ TapKitDemo/TapKit/Network/TKURLConnectionOperation.h
@@ -7,14 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "TKURLRequest.h"
 
 @interface TKURLConnectionOperation : TKOperation<
     NSURLConnectionDelegate,
     NSURLConnectionDataDelegate
 > {
   
-  TKURLRequest *_request;
+  NSString *_address;
+  NSURLRequestCachePolicy _cachePolicy;
+  NSTimeInterval _timeoutInterval;
+  NSString *_method;
+  NSDictionary *_headers;
+  NSData *_body;
   
   NSURLResponse *_response;
   NSData *_responseData;
@@ -22,17 +26,51 @@
   NSFileHandle *_responseFileHandle;
   
   NSURLConnection *_connection;
-  NSURLRequestCachePolicy _cachePolicy;
-  NSTimeInterval _timeoutInterval;
   
   NSString *_runLoopMode;
   
   BOOL _shouldUpdateNetworkActivityIndicator;
+  
+  int _bytesHandled;
+  int _totalBytesHandled;
+  int _totalBytesExpectedToHandle;
 }
 
-- (id)initWithRequest:(TKURLRequest *)request;
+@property (nonatomic, strong, readonly) NSString *address;
+@property (nonatomic, readonly) NSURLRequestCachePolicy cachePolicy;
+@property (nonatomic, readonly) NSTimeInterval timeoutInterval;
+@property (nonatomic, copy, readonly) NSString *method;
+@property (nonatomic, strong, readonly) NSDictionary *headers;
+@property (nonatomic, strong, readonly) NSData *body;
+
+@property (nonatomic, strong, readonly) NSURLResponse *response;
+@property (nonatomic, strong, readonly) NSData *responseData;
+@property (nonatomic, copy) NSString *responseFilePath;
+@property (nonatomic, strong, readonly) NSFileHandle *responseFileHandle;
+
+@property (nonatomic, strong, readonly) NSURLConnection *connection;
+
+@property (nonatomic, copy) NSString *runLoopMode;
+
+@property (nonatomic, assign) BOOL shouldUpdateNetworkActivityIndicator;
+
+@property (nonatomic, readonly) int bytesHandled;
+@property (nonatomic, readonly) int totalBytesHandled;
+@property (nonatomic, readonly) int totalBytesExpectedToHandle;
+
+
+- (id)initWithAddress:(NSString *)address
+          cachePolicy:(NSURLRequestCachePolicy)cachePolicy
+      timeoutInterval:(NSTimeInterval)timeoutInterval
+               method:(NSString *)method;
 
 - (void)startAsynchronous;
 - (NSData *)startSynchronous;
+
+- (void)addValue:(NSString *)value forRequestHeader:(NSString *)header;
+- (void)setRequestHeaders:(NSDictionary *)headers;
+
+- (void)setRequestBody:(NSData *)body;
+- (void)setFormFields:(NSDictionary *)fields;
 
 @end
--- TapKitDemo/TapKit/Network/TKURLConnectionOperation.m
+++ TapKitDemo/TapKit/Network/TKURLConnectionOperation.m
@@ -10,4 +10,63 @@
 
 @implementation TKURLConnectionOperation
 
+- (void)setFormFields:(NSDictionary *)fields
+{
+  if ( [fields count] < 1 ) {
+    return;
+  }
+  
+  NSString *boundary = [NSString UUIDString];
+  
+  
+  // Set boundary header
+  NSString *header = @"Content-Type";
+  NSString *value = [[NSString alloc] initWithFormat:@"multipart/form-data; boundary=%@", boundary];
+  [self addValue:value forHeader:header];
+  
+  
+  NSMutableData *body = [[NSMutableData alloc] init];
+  
+  
+  NSString *prefixString = [[NSString alloc] initWithFormat:@"--%@\r\n", boundary];
+  NSData *prefixData = [prefixString dataUsingEncoding:NSUTF8StringEncoding];
+  
+  NSString *suffixString = [[NSString alloc] initWithFormat:@"--%@--\r\n", boundary];
+  NSData *suffixData = [suffixString dataUsingEncoding:NSUTF8StringEncoding];
+  
+  
+  for ( NSString *name in fields ) {
+    
+    [body appendData:prefixData];
+    
+    id value = [fields objectForKey:name];
+    
+    if ( [value isKindOfClass:[NSDictionary class]] ) {
+      
+      NSString *filename = value[ @"filename" ];
+      NSData *data = value[ @"data" ];
+      
+      NSMutableString *field = [[NSMutableString alloc] init];
+      [field appendFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n", name, filename];
+      [field appendFormat:@"Content-Type: %@\r\n\r\n", [filename MIMEType]];
+      
+      [body appendData:[field dataUsingEncoding:NSUTF8StringEncoding]];
+      [body appendData:data];
+      [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+      
+    } else {
+      NSMutableString *field = [[NSMutableString alloc] init];
+      [field appendFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", name];
+      [field appendFormat:@"%@\r\n", value];
+      
+      [body appendData:[field dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+  }
+  
+  [body appendData:suffixData];
+  
+  _body = body;
+}
+
 @end
